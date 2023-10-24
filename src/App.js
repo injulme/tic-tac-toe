5c6f728fab28017708bfe1e2857e0f24c2b83c9a
injulme <injulme0309@naver.com>
2023-10-24 16:57:25 +0900

    feat: tic tac toe function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
 
-function App() {
+function Board({ squares, onClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      {squares.map((square, index) => {
+        return (
+          <button
+            className="grid-item"
+            onClick={() => onClick(index)}
+            key={index}
+          >
+            {square}
+          </button>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const initialSquares = Array(9).fill(null);
+  const [squares, setSquares] = useState([...initialSquares]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    if (winner || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  const onReset = () => {
+    setSquares(initialSquares);
+    setXIsNext(true);
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <button className="game-reset__button" onClick={onReset}>
+          다시하기
+        </button>
+        <div className="game-winner__title">{status}</div>
+        <Board squares={squares} onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
